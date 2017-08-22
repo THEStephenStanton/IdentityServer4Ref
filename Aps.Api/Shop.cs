2925b050f4149cabc8ea3721aeff1c2fa94b8e65
Stephen Stanton <stephenstanton10@gmail.com>
2017-08-22 23:33:50 +1000

    Created the alien pawn shop api

---
+++ Aps.Api/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aps.Api
+{
+    public static class Shop
+    {
+        public static IEnumerable<Item> GetItems()
+        {
+            return new List<Item>
+            {
+                new Item { Id = 0, Name = "Multi-Phase Quantum Resonator", Cost = "70 Smidgen" },
+                new Item { Id = 1, Name = "Gwendolyn", Cost = "20 Smidgen" },
+                new Item { Id = 2, Name = "Schezwan Sauce", Cost = "SOLD OUT!" }
+            };
+        }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cost { get; set; }
+    }
+}
---
+++ Aps.Api/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Aps.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            /// This middleware adds the authentication.  Its job is to validate the incoming token to make
+            /// sure that it is coming from a trusted issuerer and to make sure the token is valid
+            /// for this api (aka scope)
+            app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
+            {
+                Authority = "http://localhost:5000",
+                RequireHttpsMetadata = false,
+
+                ApiName = "apsApi"
+            });
+
+            app.UseMvc();
+        }
+    }
+}
