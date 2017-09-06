ff9ff57666190c608111b9dcaa988df357255f7d
Stephen Stanton <stephenstanton10@gmail.com>
2017-09-06 17:14:04 +1000

    Finished quickstarts

--- Aps.AuthIdentity/Startup.cs
+++ Aps.AuthIdentity/Startup.cs
@@ -12,6 +12,9 @@ using Microsoft.Extensions.Logging;
 using Aps.AuthIdentity.Data;
 using Aps.AuthIdentity.Models;
 using Aps.AuthIdentity.Services;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace Aps.AuthIdentity
 {
@@ -49,22 +52,27 @@ namespace Aps.AuthIdentity
 
             services.AddMvc();
 
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
+            var connectionString = @"server=(localdb)\mssqllocaldb;database=AlienPawnShop;trusted_connection=yes";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
-            // Adds IdentityServer
+            // configure identity server with in-memory users, but EF stores for clients and resources
             services.AddIdentityServer()
                 .AddTemporarySigningCredential()
-                .AddInMemoryIdentityResources(Resources.GetIdentityResources())
-                .AddInMemoryApiResources(Resources.GetApiResources())
-                .AddInMemoryClients(Clients.Get())
-                .AddAspNetIdentity<ApplicationUser>();
+                .AddTestUsers(Users.Get())
+                .AddConfigurationStore(builder =>
+                    builder.UseSqlServer(connectionString, options =>
+                        options.MigrationsAssembly(migrationsAssembly)))
+                .AddOperationalStore(builder =>
+                    builder.UseSqlServer(connectionString, options =>
+                        options.MigrationsAssembly(migrationsAssembly)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // this will do the initial DB population
+            InitializeDatabase(app);
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
@@ -92,6 +100,43 @@ namespace Aps.AuthIdentity
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Clients.Get())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Resources.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Resources.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
---
+++ Aps.AuthIdentity/Users.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Aps.AuthIdentity
+{
+    public static class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "rick",
+                    Password = "wubbalubba",
+                    Claims = new []
+                    {
+                        new Claim("name", "Rick Sanchez"),
+                        new Claim("website", "http://rickandmorty.wikia.com/wiki/Rick_Sanchez")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "morty",
+                    Password = "uhhh",
+                    Claims = new []
+                    {
+                        new Claim("name", "Morty Smith"),
+                        new Claim("website", "http://rickandmorty.wikia.com/wiki/Morty_Smith")
+                    }
+                }
+            };
+        }
+    }
+}
